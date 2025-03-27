927cfa3316384d905590c1f0fa12f76a9c02bf6c
nakarinnn <namekung951.15999@gmail.com>
2025-03-27 18:17:25 +0700

    fix code

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:2000',
+        target: "https://food-recipe-be-wsb1.onrender.com",
+        // target:  'http://localhost:2000',
         changeOrigin: true,
         secure: false,
       },
